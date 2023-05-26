576a762ae6c11c5e4c11950559210bb5720c26ea
Philip Pierce <pfcpiii@gmail.com>
2023-05-26 13:09:47 -0400

    first update

--- client/index.js
+++ client/index.js
@@ -6,10 +6,18 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  
+  const name = 'Tyler Schultz';
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    vName: name,
+    vProof: proof,
   });
+
 
   console.log({ gift });
 }
